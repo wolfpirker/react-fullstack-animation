a361e28f65708a24f45183ad9dd451679a234b55
wolfpirker <w_pirker@gmx.de>
2021-12-13 22:41:39 +0100

    CSSTransitions using classes and css

--- src/components/CSStransition.js
+++ src/components/CSStransition.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
+import { CSSTransition } from 'react-transition-group';
 
-class CssTr extends Component{
-    render(){
-        return(
+class CssTr extends Component {
+
+    state = {
+        show: true
+    }
+
+    showDiv = () => {
+        this.setState({
+            show: !this.state.show
+        })
+    }
+
+    render() {
+        return (
             <div>
-                CSS TRANSITIONS
+                <CSSTransition
+                    in={this.state.show}
+                    timeout={500}
+                    classNames="cssSq"
+                >
+                    <div className={`cssSq ${this.state.show}`}>
+                        hello
+                    </div>
+                </CSSTransition>
+                <button
+                    onClick={this.showDiv}
+                >
+                    toggle it
+                </button>
             </div>
         )
     }
